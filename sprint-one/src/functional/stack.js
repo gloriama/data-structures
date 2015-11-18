943fd874af5f2e046ed5101ad660717f1e4c0bb0
gloriama <gloria.ma@aya.yale.edu>
2015-11-18 15:03:33 -0800

    Implements incrementing stack size

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,15 +4,18 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var stackSize = 0;
+
   // Implement the methods below
   someInstance.push = function(value) {
+    stackSize++;
   };
 
   someInstance.pop = function() {
   };
 
   someInstance.size = function() {
-    return 0; 
+    return stackSize; 
   };
 
   return someInstance;
