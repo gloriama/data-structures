18929633e225a2115ed07bde70c1f30611c54d7b
gloriama <gloria.ma@aya.yale.edu>
2015-11-18 15:22:53 -0800

    Passes 5th test for functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -13,7 +13,9 @@ var Queue = function() {
   };
 
   someInstance.dequeue = function() {
-    queueSize--;
+    if(queueSize > 0){
+      queueSize--;
+    }
   };
 
   someInstance.size = function() {
