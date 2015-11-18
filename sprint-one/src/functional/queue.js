0b556888f4581379c078d3a4c630b817a20ddcd1
gloriama <gloria.ma@aya.yale.edu>
2015-11-18 15:41:10 -0800

    Completes functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,16 +5,24 @@ var Queue = function() {
   var storage = {};
 
   var queueSize = 0;
+  var headIndex = 0; // points to key of next item to dequeue
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[queueSize + headIndex] = value;
     queueSize++;
   };
 
   someInstance.dequeue = function() {
-    if(queueSize > 0){
-      queueSize--;
+    if(queueSize === 0){
+      return undefined; 
+    } else{
+      var returnVal = storage[headIndex];
+      delete storage[headIndex];
+      headIndex++;
+      queueSize--;  
+      return returnVal;
     }
   };
 
