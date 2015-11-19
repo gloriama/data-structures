6ee58cbf55481cdaae82a748e45a1e800889f2ab
gloriama <gloria.ma@aya.yale.edu>
2015-11-18 16:22:47 -0800

    Created scaffolding for functional-shared stack Passes first test for functional-shared stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var stack = {};
+
+  stack.stackSize = 0;
+  stack.storage = {};
+  _.extend(stack, stackMethods);
+  
+  return stack;
 };
 
 var stackMethods = {};
 
+//stackMethods.push = a function that adds a value to the stack (which will be accessed by this)
+//stackMethods.pop = a function that removes a value from the stack (same as above)
+//stackMethods.size = a function that returns the size of the stack (same as above)
 
+stackMethods.push = function(value) {
+
+};
+
+stackMethods.pop = function() {
+
+};
+
+stackMethods.size = function() {
+  return 0;
+};
