5a996ac355ae0f52c0b0bacbaaf4cee3592a39ce
gloriama <gloria.ma@aya.yale.edu>
2015-11-18 21:37:18 -0800

    Completes prototypal queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,35 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var queue = Object.create(queueMethods);
+
+  queue.queueSize = 0; 
+  queue.storage = {};
+  queue.headIndex = 0;
+
+  return queue;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.queueSize + this.headIndex] = value;
+  this.queueSize++;
+};
 
+queueMethods.dequeue = function() {
+  if (this.queueSize === 0) {
+    return undefined;
+  } else {
+    var returnVal = this.storage[this.headIndex];
+    delete this.storage[this.headIndex];
+    this.headIndex++;
+    this.queueSize--;
+    return returnVal;
+  }
+};
+
+queueMethods.size = function() {
+  return this.queueSize;
+};
