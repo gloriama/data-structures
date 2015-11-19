089c46f350a67eeb2b9af8736e2004192e082af9
gloriama <gloria.ma@aya.yale.edu>
2015-11-18 16:35:30 -0800

    Completes functional-shared stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -18,12 +18,18 @@ var stackMethods = {};
 //stackMethods.size = a function that returns the size of the stack (same as above)
 
 stackMethods.push = function(value) {
+  this.storage[this.stackSize] = value;
   this.stackSize++;
 };
 
 stackMethods.pop = function() {
-  if (this.stackSize > 0) {
+  if (this.stackSize === 0) {
+    return undefined;
+  } else {
+    var returnVal = this.storage[this.stackSize-1];
+    delete this.storage[this.stackSize-1];
     this.stackSize--;
+    return returnVal;
   }
 };
 
