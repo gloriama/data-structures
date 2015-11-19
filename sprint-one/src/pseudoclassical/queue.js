b70149bb431e92eb382e73db4aeb94910fd31a4f
gloriama <gloria.ma@aya.yale.edu>
2015-11-18 21:50:13 -0800

    Completes pseudoclassical

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.queueSize = 0;
+  this.storage = {};
+  this.headIndex = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.queueSize + this.headIndex] = value;
+  this.queueSize++;
+};
 
+Queue.prototype.dequeue = function() {
+  if (this.queueSize === 0) {
+    return undefined;
+  } else {
+    var returnVal = this.storage[this.headIndex];
+    delete this.storage[this.headIndex];
+    this.headIndex++;
+    this.queueSize--;
+    return returnVal;
+  }
+};
+
+Queue.prototype.size = function() {
+  return this.queueSize;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.stackSize = 0;
+  this.storage = {};
 };
+
+Stack.prototype.push = function(value){
+  this.storage[this.stackSize] = value;
+  this.stackSize++;
+};
+
+Stack.prototype.pop = function(){
+  if (this.stackSize === 0){
+    return undefined;
+  } else {
+    var returnVal = this.storage[this.stackSize - 1];
+    delete this.storage[this.stackSize - 1];
+    this.stackSize--;
+    return returnVal;
+  }
+};
+
+Stack.prototype.size = function(){
+  return this.stackSize;
+};
+
+
 
 
