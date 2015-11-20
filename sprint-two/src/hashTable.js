3f9d9018e6bc65bea7e22e06278aeaa2f79666ee
gloriama <gloria.ma@aya.yale.edu>
2015-11-19 20:09:35 -0800

    Completes hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,19 +7,50 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+  //this._storage.set(index, v);
 
+  // first time we add k:v to storage at an index
+    // create an array and store {k:v} at the 0th index
+  // push {k:v} to the array at the index
+  
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, []); 
+  }
+  
+  var item = {};
+  item[k] = v;
+  this._storage.get(index).push(item);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  //return this._storage.get(index);
+
+  var result = undefined;
+  var bucket = this._storage.get(index);
+  _.each(bucket, function(item) {
+    if (k in item) {
+      result = item[k];
+    }
+  });
+  return result;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   //use 'set' to set index of this._storage to undefined
-  this._storage.set(index, undefined);
+  //this._storage.set(index, undefined);
+
+  //var bucket = array at index in storage
+  //loop through bucket, with callback that takes item, index:
+    //if k in item
+      //bucket.splice with index,1
+  var bucket = this._storage.get(index);
+  _.each(bucket, function(item, index) {
+    if (k in item) {
+      bucket.splice(index, 1);
+    }
+  });
 };
 
 
