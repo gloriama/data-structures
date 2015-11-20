c2354fbf5ea0cfb4d06f8688be2baefd34d66246
gloriama <gloria.ma@aya.yale.edu>
2015-11-19 16:13:32 -0800

    Completes graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -53,6 +53,9 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
 // ------------------------
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  _.each(this.storage, function(neighborsObj, nodeVal) {
+    cb(nodeVal);
+  });
 };
 
 /*
