c92c873bca3b5dc5ca3f796a0a8c1b07df0ea46f
gloriama <gloria.ma@aya.yale.edu>
2015-11-21 11:42:06 -0800

    Completes advanced tree Includes test spec and test runner for advanced tree

---
+++ advanced/src/tree-advanced.js
@@ -0,0 +1,60 @@
+var Tree = function(value) {
+  var newTree = {};
+
+  newTree.value = value;
+  newTree.children = [];
+  newTree.parent = null;
+
+  _.extend(newTree, treeMethods);
+
+  return newTree;
+};
+
+var treeMethods = {};
+
+treeMethods.addChild = function(value) {
+  var child = Tree(value); // creates new tree with value as tree's value
+  this.children.push(child);
+  child.parent = this;
+};
+
+treeMethods.removeFromParent = function() {
+  this.parent.children = _.reject(this.parent.children, function(child) {
+    return child === this;
+  });
+  this.parent = null;
+};
+
+treeMethods.contains = function(target) {
+  //(a longer version, saved for fun)
+
+  // var found = false;
+
+  // var search = function(root){
+  //   if (root.value === target){
+  //     found = true;
+  //   } else {
+  //     _.each(root.children, function(child){
+  //       search(child);
+  //     });
+  //   }
+  // }
+  // search(this);
+  // return found;
+
+  //==============
+
+  return (this.value === target) ||
+         _.some(this.children, function(child) { return child.contains(target); });
+
+};
+
+
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ */
+
+//addChild: O(1)
+//removeFromParent: O(n)
+//contains: O(n)
